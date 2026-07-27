c6be409e0600073447e7161174d4ba953a80063e
Andrew White <andrewh@cdw.com>
2026-07-27 07:20:05 -0500

    fix(setup-fio): ssh options placed after host in drop_caches (+1 more)

--- bobber/test_scripts/setup_fio.sh
+++ bobber/test_scripts/setup_fio.sh
@@ -18,7 +18,7 @@ drop_caches () {
 		declare -a pidlist
 		unset pidlist
 		for N in ${FIO_NODELIST}; do
-            ssh $N $SSHOPTS /sbin/sysctl vm.drop_caches=3 &
+            ssh ${SSHOPTS} $N /sbin/sysctl vm.drop_caches=3 &
 		    p=$!
 		    pidlist=(${pidlist[@]} $p)
         done
@@ -30,7 +30,7 @@ drop_caches () {
 stop_servers () {
 
     declare -a pidlist
-    pidlist=""
+    pidlist=()
     for N in $FIO_NODELIST; do
         echo "Killing Server on $N"
 
